c531095d0191f6947f7139b1191e8fb645ca6a10
rajshekar <rajshekar.mudhigonda2468@gmail.com>
2022-11-22 17:33:37 +0530

    ExpressServerStarter

---
+++ javascript-server/express-server/server.js
@@ -0,0 +1,23 @@
+import express  from 'express';
+
+
+const PORT = 8080;
+const server = new express();
+
+server.use(express.json());
+server.get('/',(req,res)=>{
+    res.status(200).send({name:'chatapp',place:'nagaram'});
+});
+
+server.get('/asteriods',(req,res)=>{
+        const paramters = req.query;
+        console.log(paramters);
+        console.log(paramters.id);
+        res.send(paramters.id);
+});
+
+
+server.listen(PORT,console.log(`Listening to Port:${PORT}`)); 
+        //async function listen(PORT,callback)
+
+console.log('hello'); 
