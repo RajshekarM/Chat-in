7dbb6edfb7a26e0c7993529964cb5bdd9b71751f
rajshekar <rajshekar.mudhigonda2468@gmail.com>
2022-11-22 12:21:20 +0530

    Added File System

--- javascript-server/nodeCommands.js
+++ javascript-server/nodeCommands.js
@@ -2,18 +2,42 @@
 console.log(global.process.platform) //tell OS in your machine
 const environment = global.process.env;
 
-//Events : Event loop : Non blocking Operations on Main Thread and long asnchronous process is given to seperate thread. 
+//Events : Event loop : Non blocking Operations runs on Main Thread and long asnchronous process is given to seperate thread. 
 
 const {EventEmitter} = require('events')
 const eventEmitter =  new EventEmitter();
 eventEmitter.on('sleeptime',()=>console.log('Time to go to bed'));
 
+var timeInterval = 100;
 setInterval(()=>{
     const hour = new Date().getHours();
-    if(hour>=9)
+    if(hour>=23)
     eventEmitter.emit('sleeptime');
+    console.log(timeInterval);
                 },
-1000
+                timeInterval
 );
+timeInterval = 5000;
 
+
+
+
+/**************************File System****************************/
+/*const {readFile,readFileSync} = require('fs');
+let reader = 'hello';
+readFile('./hello.txt','utf-8',(err,txt)=>{
+    reader = txt;
+    console.log('file read: loading into data struture');
+   
+});
+setTimeout(()=>console.log(reader),5000);*/
+
+//With the help of promises
+const {readFile} = require('fs').promises;
+
+async function read(){
+        const file = await readFile('./hello.txt','utf8');
+        console.log(file);
+}
+read()
 
