2a8b4d8e486440b3bc3b9fbc8e4e387a6400de0e
rajshekar <rajshekar.mudhigonda2468@gmail.com>
2022-11-23 16:24:43 +0530

    Added controller for login

--- javascript-server/httpserver-express/routes/serviceRoute.js
+++ javascript-server/httpserver-express/routes/serviceRoute.js
@@ -3,9 +3,8 @@ import express from 'express';
 
 const serviceRoute = express.Router();
 
-
+//adding callback to service path
+//callbacks will be added to stack/map({key:path,value:callback} for faster access)
 serviceRoute.get('/',(req,res)=>{res.send('Choose your service')});
 
-export default serviceRoute;
-
-
+    export default serviceRoute;
